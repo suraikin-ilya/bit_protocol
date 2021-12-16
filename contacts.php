db0568491da0309cf3e521fa5538b06136d7f73b
suraikin-ilya <ilya.suraikin@mail.ru>
2021-12-17 00:02:14 +0300

    contact page

---
+++ contacts.php
@@ -0,0 +1,34 @@
+<?php
+$db_connect = mysqli_connect("std-mysql", "std_1252_bit", "12345678", "std_1252_bit")
+or die("Ошибка " . mysqli_error($db_connect));
+mysqli_set_charset($db_connect, "utf8");
+$_SERVER['SERVER_NAME'] = 'localhost:63342/bit_protocol';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>BIT Quiz </title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/authentication/form-1.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/theme-checkbox-radio.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/forms/switches.css">
+</head>
+<body class="form">
+
+<div class="container">
+
+
+
+
+    <link rel="stylesheet" href="styles.css">
+    <body class="text-center">
