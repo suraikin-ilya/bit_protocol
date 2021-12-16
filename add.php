2b12d80dd41bb0a85bcca173b0e7c61a8a51d050
suraikin-ilya <ilya.suraikin@mail.ru>
2021-12-16 20:42:02 +0300

    add form page

---
+++ add.php
@@ -0,0 +1,65 @@
+<?php
+$db_connect = mysqli_connect("std-mysql", "std_1252_bit", "12345678", "std_1252_bit")
+or die("Ошибка " . mysqli_error($db_connect));
+mysqli_set_charset($db_connect, "utf8");
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>BIT Admin</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <link href="assets/css/loader.css" rel="stylesheet" type="text/css" />
+    <script src="assets/js/loader.js"></script>
+
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+</head>
+<?php
+if($_COOKIE['user']== ''):
+    header('Location: ../bit_protocol/auth_login.php');
+    ?>
+<?php else: ?>
+<body class="form">
+    <div class="form-container">
+
+
+
+
+
+</div>
+
+<script src="assets/js/libs/jquery-3.1.1.min.js"></script>
+<script src="bootstrap/js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="assets/js/app.js"></script>
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+<script src="assets/js/custom.js"></script>
+<!-- END GLOBAL MANDATORY SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+<script src="plugins/apex/apexcharts.min.js"></script>
+<script src="assets/js/dashboard/dash_2.js"></script>
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+</body>
+</html>
+<?php endif; ?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$db_connect = mysqli_connect("std-mysql", "std_1252_bit", "12345678", "std_1252_bit")
+or die("Ошибка " . mysqli_error($db_connect));
+mysqli_set_charset($db_connect, "utf8");
+
 
 if($_GET['action'] == 'logout') {
 
@@ -133,12 +137,51 @@ if($_COOKIE['user']== ''):
 
     <!--  BEGIN CONTENT PART  -->
     <div id="content" class="main-content">
-        
+        <div class="layout-px-spacing mb-4">
+
+            <div class="table-">
+                <table class="table table-bordered table-hover table-striped table-checkable table-highlight-head mb-4">
+                    <thead>
+                    <tr>
+                        <th class="">Project Name</th>
+                        <th class="">Link</th>
+                        <th class="text-center">Icons</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>
+                            <p class="mb-0">Shaun Park</p>
+                        </td>
+                        <td>320</td>
+                        <td class="text-center">
+                            <ul class="table-controls">
+                                <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="" data-original-title="Settings"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings text-primary"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg></a> </li>
+                                <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 text-success"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a></li>
+                                <li><a href="javascript:void(0);" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 text-danger"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+            <div class="col-xl-12 col-md-4">
+                <a href="add.php" class="btn btn-success btn-download">Add-project</a>
+            </div>
+        </div>
     </div>
     <!--  END CONTENT PART  -->
 
 </div>
 <!-- END MAIN CONTAINER -->
+<body class="form">
+
+
+<div class="form-container">
+
+
+
 
 <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
 <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
