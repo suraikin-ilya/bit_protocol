2c088e190f50e3169e797424a2cc537984b8426c
suraikin-ilya <ilya.suraikin@mail.ru>
2022-02-21 18:56:31 +0300

    password fix

--- includes/auth.php
+++ includes/auth.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 1);
 
 $username = $_POST['username'];
 
-$password = $_POST['password'];
+$password = md5($_POST['password']);
 
 $check_user = mysqli_query($db_connect, "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'");
 $user = mysqli_fetch_assoc($check_user);
--- includes/register.php
+++ includes/register.php
@@ -15,7 +15,7 @@ require "db_connection.php";
 
 $username = $_POST['username'];
 $email = $_POST['email'];
-$password = $_POST['password'];
+$password = md5($_POST['password']);
 
 
 
